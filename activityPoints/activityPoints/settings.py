42db23b596afa4bf676c7779980c1f3af3fccb27
TusharKumarChopra <tusharkumarchopra@gmail.com>
2024-07-09 22:32:30 +0530

    Added navbar to announcement_detail

--- activityPoints/activityPoints/settings.py
+++ activityPoints/activityPoints/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k9_0u2(3z7d7kh9hxpf!(god8q9ok)90e_c0n8v5!7kbs%ui*0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -121,10 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# settings.py
 
+# Define the directory where your static files are located
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static'),  # Adjust 'static' to your actual directory name
 ]
 
 
